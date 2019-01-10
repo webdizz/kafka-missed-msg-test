3faf40c560af8598457b6ddd033fd93ac42d4310
Izzet Mustafaiev <izzet@mustafaiev.com>
2019-01-10 19:28:21 +0200

    Add admin client to check current configuration of topic

---
+++ kafka-admin.groovy
@@ -0,0 +1,21 @@
+import org.apache.kafka.clients.admin.*
+import org.apache.kafka.common.config.*
+import groovy.json.JsonOutput
+import groovy.json.JsonBuilder
+
+@Grab(group = "org.apache.kafka", module = "kafka-clients", version = "2.0.0")
+//@Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.1.2')
+
+Properties props = new Properties()
+props.put('zk.connect', 'localhost:2181')
+String kafkaHost = System.env['KAFKA_HOSTS']
+props.put('bootstrap.servers', kafkaHost)
+
+String topic = System.env['KAFKA_TOPIC'] ?: 'test'
+
+AdminClient adminClient = AdminClient.create(props)
+
+DescribeConfigsResult configs = adminClient.describeConfigs([new ConfigResource(ConfigResource.Type.TOPIC, topic)])
+
+def obtainedConfig = configs.all().get()
+println obtainedConfig
